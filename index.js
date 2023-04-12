c2476cf29c962bbf8ee8c1451a42d51ebfe4c31a
jaydeeptalsania <jaydeept007@gmail.com>
2023-04-12 10:55:36 +0530

    Example of inbuilt module (file system)

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const msg = 'Hello';
-console.log(msg);  // to run this file type 'node index.js' in terminal
+const fs = require('fs');
+
+const fileData = fs.readFileSync('./starter/txt/input.txt','utf-8');
+console.log(fileData);
+
+const writeData = `Hello I am writing into this file on ${Date.now()}`;
+fs.writeFileSync('./starter/txt/output.txt' , writeData);
+console.log('File has been created');
