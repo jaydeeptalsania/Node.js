907f4d4e572f7e97edd02387f5e3f6d5a8bccc1e
jaydeeptalsania <jaydeept007@gmail.com>
2023-04-21 16:09:45 +0530

    Use of slugify(third-party) module

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const slugify =  require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 /*const fileData = fs.readFileSync('./starter/txt/input.txt','utf-8');
 console.log(fileData);
@@ -32,6 +34,9 @@ const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-pro
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`,'utf-8');
 const dataObj = JSON.parse(data);  // convert json into javascript object
 
+//console.log(slugify('Hello World',{replacement:'-',lower:true}));  // example of slugify module
+  const slugs = dataObj.map(el => (slugify(el.productName,{replacement:'-',lower:true})));
+  console.log(slugs);
 
 const server = http.createServer((req,res)=>{
    const {query , pathname} = url.parse(req.url,true);
