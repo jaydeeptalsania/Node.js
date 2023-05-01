16f646f388862acdde3750c0410289584bd6c92f
jaydeeptalsania <jaydeept007@gmail.com>
2023-05-01 10:00:52 +0530

    callback hell

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const superagent = require('superagent'); 
+
+fs.readFile(`${__dirname}/dog.txt`,'utf-8',(err,data)=>{
+    //console.log(data);
+    if(err) return console.log(err.message);
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err,res)=>{
+       console.log(res.body);
+
+       fs.writeFile(`dog-img.txt`,res.body.message,err=>{
+             console.log('dog image saved!');
+       });
+    });
+}); 
