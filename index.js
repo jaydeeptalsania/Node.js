bb608f03ec82bc7e2c97ca19fae2a78cfc49c1c0
jaydeeptalsania <jaydeept007@gmail.com>
2023-05-01 10:15:56 +0530

    make use of promises

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const superagent = require('superagent'); 
 
-fs.readFile(`${__dirname}/dog.txt`,'utf-8',(err,data)=>{
+/*fs.readFile(`${__dirname}/dog.txt`,'utf-8',(err,data)=>{
     //console.log(data);
     if(err) return console.log(err.message);
     superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err,res)=>{
@@ -11,4 +11,15 @@ fs.readFile(`${__dirname}/dog.txt`,'utf-8',(err,data)=>{
              console.log('dog image saved!');
        });
     });
-}); 
+});*/
+
+ fs.readFile(`${__dirname}/dog.txt` , 'utf-8',(err,data)=>{
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).then(res=>{
+        console.log(res.body);
+        fs.writeFile(`dog-img.txt`,res.body.message,err =>{
+              console.log('dog image saved!');
+        });
+    }).catch(err=>{
+        console.log(err.message);
+    })
+ });
