057d9922dfc2b4b9779bc3c74826e1b94afb5286
jaydeeptalsania <jaydeept007@gmail.com>
2023-05-02 10:53:33 +0530

    using post method

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
     res.status(200).json({message:'Hello from the server', app:'natours'});
 });
 
+app.post('/',(req,res)=>{
+   res.status(200).send('You can post at this endpoint...');
+})
+
 const port = 3000;
 app.listen(port,()=>{
     console.log(`App is running on ${port} port`);
