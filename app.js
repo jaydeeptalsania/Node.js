3bb2289f2a3ad9034ad015f31391a0a8eec3522f
jaydeeptalsania <jaydeept007@gmail.com>
2023-05-03 11:17:58 +0530

    api post request

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+app.use(express.json());  // added middleware for post data (req.body)
+
 /*app.get('/',(req,res)=>{
   //res.status(200).send('Hello from the server');
     res.status(200).json({message:'Hello from the server', app:'natours'});
@@ -21,7 +23,23 @@ app.get('/api/v1/tours',(req,res)=>{
        tours:tours
      }
    })
-})
+});
+
+app.post('/api/v1/tours',(req,res)=>{
+    //console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id:newId},req.body);  // merge two objects into a single object
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err =>{
+        res.status(201).json({
+          status:'success',
+          data:{
+            tour:newTour
+          }
+        })
+    });
+});
 
 const port = 3000;
 app.listen(port,()=>{
