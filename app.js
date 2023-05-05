76392b90b61cc428d776c1f7abba3ea2055e4a97
jaydeeptalsania <jaydeept007@gmail.com>
2023-05-05 09:34:53 +0530

    patch api example

--- app.js
+++ app.js
@@ -62,6 +62,23 @@ app.post('/api/v1/tours',(req,res)=>{
     });
 });
 
+app.patch('/api/v1/tours/:id',(req,res)=>{
+  if(req.params.id * 1 > tours.length){
+     return res.status(404).json({
+       status:'fail',
+       message:'Invalid Id'
+     });
+  }
+
+  res.status(200).json({
+    status:'success',
+    data:{
+      tour:`<Updated tour number:- ${req.params.id}>`
+    }
+  })
+
+});
+
 const port = 3000;
 app.listen(port,()=>{
     console.log(`App is running on ${port} port`);
